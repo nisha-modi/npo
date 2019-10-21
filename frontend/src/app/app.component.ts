91bc033057413c892ea6ceb99b3fdd06d79b805a
Steffen Olson <solson@reliaslearning.com>
2019-10-21 14:13:44 -0400

    use azure functions for backend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment as env } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  funcResponse$ = this.http.get(`${env.api}/Ping?name=world`, { responseType: 'text' });
+
+  constructor(private http: HttpClient) {}
+}
