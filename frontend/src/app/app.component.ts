f91e46fe987bd166542875e941e06d23d76a0fea
Nisha Modi <34256110+nisha-modi@users.noreply.github.com>
2019-10-21 15:46:16 -0400

    Added logo to page!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { environment as env } from 'src/environments/environment';
+import { environment as env } from "src/environments/environment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Node Package Optimizer';
+  appLogoUrl = "assets/app-logo.png";
 
-  funcResponse$ = this.http.get(`${env.api}/Ping?name=world`, { responseType: 'text' });
+  funcResponse$ = this.http.get(`${env.api}/Ping?name=world`, {
+    responseType: "text"
+  });
 
   constructor(private http: HttpClient) {}
 }
