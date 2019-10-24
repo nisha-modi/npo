7148d269c85a849649cf53ff84c0d62c77cc40ed
Nisha Modi <34256110+nisha-modi@users.noreply.github.com>
2019-10-24 16:13:51 -0400

    added methods to modify dependency array

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -84,29 +84,4 @@ export class AppComponent implements OnInit {
     });
     this.state = 'analyze';
   }
-
-  removeLowScoringDependencies(dependencies: Dependency[]) {
-    for (let i = dependencies.length - 1; i <= 0; i--) {
-      if (dependencies[i].dependencyScore <= 1) {
-        dependencies.splice(i, 1);
-      }
-    }
-    return dependencies;
-  }
-
-  removeWorseAlternatives(dependencies: Dependency[]) {
-    for (let i = dependencies.length - 1; i >= 0; i--) {
-      for (let j = dependencies[i].alternatives.length - 1; j <= 0; j--) {
-        if (
-          dependencies[i].treeScore > dependencies[i].alternatives[j].treeScore
-        ) {
-          dependencies[i].alternatives.splice(j, 1);
-        }
-      }
-      if (dependencies[i].alternatives.length == 0) {
-        dependencies.splice(i, 1);
-      }
-    }
-    return dependencies;
-  }
 }
