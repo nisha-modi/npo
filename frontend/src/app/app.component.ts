e1e683d02063b4eaa5f9662e66c9bf6a17a9d6dd
Steffen Olson <solson@reliaslearning.com>
2019-10-24 20:22:23 -0400

    fix compilation issues

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,73 +8,10 @@ import { Dependency } from './models/dependency.model';
 })
 export class AppComponent implements OnInit {
   appLogoUrl = 'assets/NPO.png';
-  testAlternatives: Dependency[] = [
-    new Dependency(
-      'alternative 1',
-      'version',
-      'description',
-      5,
-      5,
-      5,
-      5,
-      5,
-      '95%',
-      []
-    ),
-    new Dependency(
-      'alternative 2',
-      'version',
-      'description',
-      5,
-      5,
-      5,
-      5,
-      5,
-      '95%',
-      []
-    ),
-    new Dependency(
-      'alternative 3',
-      'version',
-      'description',
-      5,
-      5,
-      5,
-      5,
-      5,
-      '95%',
-      []
-    )
-  ];
 
   state: 'begin' | 'analyze' = 'begin';
 
-  packageDependencies: Dependency[] = [
-    new Dependency(
-      'Dependency 1',
-      'version',
-      'description',
-      5,
-      5,
-      5,
-      5,
-      5,
-      '95%',
-      this.testAlternatives
-    ),
-    new Dependency(
-      'Dependency 2',
-      'version',
-      'description',
-      5,
-      5,
-      5,
-      5,
-      5,
-      '95%',
-      this.testAlternatives
-    )
-  ];
+  packageDependencies: Dependency[];
 
   ngOnInit() {}
 
