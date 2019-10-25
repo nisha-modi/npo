190b6edf87aef196e7458f15020aaee22b15e6c1
Steffen Olson <solson@reliaslearning.com>
2019-10-24 22:18:47 -0400

    remove temporary component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DependencyScoreComponent } from './dependency-score/dependency-score.component';
 import { DependencyAndAlternativesComponent } from './dependency-and-alternatives/dependency-and-alternatives.component';
-import { DepTreeTempComponent } from './dep-tree-temp/dep-tree-temp.component';
 import { EmptyStateComponent } from './empty-state/empty-state.component';
 import { AnalyzeComponent } from './analyze/analyze.component';
 import { DragDirective } from './dragDrop.directive';
@@ -15,7 +14,6 @@ import { DragDirective } from './dragDrop.directive';
     AppComponent,
     DependencyScoreComponent,
     DependencyAndAlternativesComponent,
-    DepTreeTempComponent,
     EmptyStateComponent,
     AnalyzeComponent,
     DragDirective
